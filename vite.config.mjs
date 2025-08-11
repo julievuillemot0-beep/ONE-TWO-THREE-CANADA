e6afdf9c9df626af77f897b6c58952f8f9608852
julievuillemot0-beep <julie.vuillemot0@gmail.com>
2025-08-11 17:43:24 +0400

    Add files via upload

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <<< CHEMIN RELATIF POUR VERCEL
+  base: './', // chemin relatif pour Vercel
 })
